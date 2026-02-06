366322338e87f68fc37583ead53494ee4ae2f10e
air-plus <jyin1221@gmail.com>
2026-02-06 12:39:05 +0800

    first commit

--- zsh/.config/zsh/aliases.sh
+++ zsh/.config/zsh/aliases.sh
@@ -15,7 +15,7 @@ alias lsf="ls -F | rg -v /"
 
 alias ftree="eza --icons --tree --level=3 -I 'node_modules|.git' | fzf --height=40% --border --ansi"
 alias recent="eza -lah --sort=modified | head -20"
-alias bigfiles="eza lah --sort=size | tail -15 | head -11"
+alias bigfiles="eza -lah --sort=size | tail -15 | head -11"
 
 alias ff="fd --type f | fzf --height=40% --border --preview 'bat --color=always --line-range :50 {}'"
 alias gba="git branch -a | fzf --preview 'git log --oneline {} --color=always' | sed 's/.* //' | xargs git checkout"
