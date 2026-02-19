aa49fcd312b0fb1915a6753edd2e22b15e5ae3a1
air-plus <jyin1221@gmail.com>
2026-02-19 09:30:56 +0800

    first commit

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,20 +1,12 @@
-abbr -a ls 'eza --icons --group-directories-first --color=always --time-style=long-iso'
-abbr -a ll 'ls -lh'
-abbr -a la 'll -a'
-abbr -a lg 'la --git --git-ignore'
+if type -q eza
+    set -l eza_base 'eza --icons --group-directories-first --color=always --time-style=long-iso'
 
-abbr -a tree 'eza --icons --tree --level=2 --color=always'
-abbr -a tree4 'eza --icons --tree --level=4 --color=always'
-abbr -a lstr 'eza --icons --tree --level=3 -I "node_modules|.git|dist|build|__pycache__|.DS_Store" --color=always'
-
-abbr -a llt 'll -s modified'
-abbr -a lls 'll -s size'
-abbr -a lsd 'ls -d */'
-abbr -a lsf 'ls -F | string match -v "*/"'
-
-abbr -a ftree 'eza --icons --tree --level=3 -I "node_modules|.git" | fzf --height=40% --border --ansi'
-abbr -a recent 'eza -lah --sort=modified | head -20'
-abbr -a bigfiles 'eza -lah --sort=size | tail -15 | head -11'
-
-abbr -a ff 'fd -t f | fzf --height=40% --border --preview "bat --color=always --line-range :50 {}"'
-abbr -a gba 'git branch -a | fzf --preview "git log --oneline --color=always {}" | string trim | sed "s/.* //" | xargs git switch'
+    abbr -a ls "$eza_base"
+    abbr -a ll "$eza_base -lh"
+    abbr -a la "$eza_base -lha"
+    abbr -a lg "$eza_base -lha --git"
+else
+    abbr -a ls 'ls -G'
+    abbr -a ll 'ls -lh'
+    abbr -a la 'ls -lha'
+end
