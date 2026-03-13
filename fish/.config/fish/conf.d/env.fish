e4c8124a2959907a64f89e86aee003220d66bbfd
air-plus <jyin1221@gmail.com>
2026-03-13 18:43:07 +0800

    first commit
    Co-authored-by: air-plus <jyin1221@gmail.com>

---
+++ fish/.config/fish/conf.d/env.fish
@@ -0,0 +1,6 @@
+set -gx EDITOR 'hx'
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
+set -g fish_transient_prompt 1
