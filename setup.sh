fc6c349a49ecb8aae588d06640c019147c95954d
air-plus <jyin1221@gmail.com>
2026-03-15 07:40:23 +0800

    first commit
    Co-authored-by: air-plus <jyin1221@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+pkg install -y termux-api fish git-delta gh fastfetch starship eza zoxide bat fd ripgrep fzf jq htop yazi file stow lazygit helix helix-grammars zellij build-essential nodejs-lts
 stow -vt $HOME */ --adopt
 bat cache --build
 mkdir -p $HOME/projects && touch $HOME/.hushlogin
