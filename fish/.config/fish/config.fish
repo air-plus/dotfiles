535ff9b3b2dd60bb75725b5e2eca49efa96b50fa
air-plus <jyin1221@gmail.com>
2026-03-22 10:31:56 +0800

    first commit
    Co-authored-by: air-plus <jyin1221@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
 
+    fastfetch -c examples/10
     starship init fish | source
 end
