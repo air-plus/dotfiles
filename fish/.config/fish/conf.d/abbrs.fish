3073d3fac17f95a45892e029eb5aaa96c08c1648
air-plus <jyin1221@gmail.com>
2026-03-30 18:37:43 +0800

    first commit
    Co-authored-by: air-plus <jyin1221@gmail.com>

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,14 +1,9 @@
 if status is-interactive
-    if command -q eza
-        set -l eza_base 'eza --icons --group-directories-first --color=always --time-style=long-iso'
+    set -l eza_base 'eza --icons --group-directories-first --color=always --time-style=long-iso'
 
-        abbr -a ls "$eza_base"
-        abbr -a ll "$eza_base -lh"
-        abbr -a la "$eza_base -lha"
-        abbr -a lg "$eza_base -lha --git --git-ignore"
-        abbr -a tree 'eza --tree --icons -I "node_modules|.git|target|vendor|dist|.cache|__pycache__"'
-    else
-        abbr -a ll 'ls -lh'
-        abbr -a la 'ls -lha'
-    end
+    abbr -a ls "$eza_base"
+    abbr -a ll "$eza_base -lh"
+    abbr -a la "$eza_base -lha"
+    abbr -a lg "$eza_base -lha --git --git-ignore"
+    abbr -a tree 'eza --tree --icons -I "node_modules|.git|target|vendor|dist|.cache|__pycache__"'
 end
