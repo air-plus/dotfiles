a9af5a9ddc8275cba641da683c5eb5badc9f2609
air-plus <jyin1221@gmail.com>
2026-04-19 17:10:16 +0800

    first commit

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     zoxide init fish | source # Zoxide 集成
 
-    fastfetch -c examples/10 # 开屏 fastfetch
+    fastfetch -c examples/10.jsonc # 开屏 fastfetch
     starship init fish | source # 初始化 Starship
 end
