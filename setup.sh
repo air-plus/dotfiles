7526da2050f83fe41c23be3443d3f6ff2861a5d7
air-plus <jyin1221@gmail.com>
2026-04-19 08:00:36 +0800

    first commit

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ git config --global --unset user.signingkey && git config --global --unset commi
 pkg install -y termux-api git fish git-delta fastfetch eza zoxide bat fd ripgrep starship fzf jq htop yazi file stow lazygit neovim zellij build-essential nodejs-lts tur-repo glibc-repo
 stow -t $HOME */ --adopt
 bat cache --build
-mkdir -p $HOME/projects && touch $HOME/.hushlogin
+mkdir -p $HOME/{projects,.img} && touch $HOME/.hushlogin
+curl -o $HOME/.img/back.png https://imgs.search.brave.com/rb92mgsxpjQCAweuBbUbTjlBQS9VFUMMX6EXsVK0DKc/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvMTIy/NTk3NDEuanBn
 chsh -s fish
