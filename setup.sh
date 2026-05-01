e66821ec515773e99424d9af138d1ef5f7ca10bd
Air Plus <jyin1221@gmail.com>
2026-05-02 06:46:02 +0800

    Refactor setup.sh to improve error handling

--- setup.sh
+++ setup.sh
@@ -1,24 +1,18 @@
 #!/usr/bin/env bash
 set -e
-
-error_exit() {
-  echo "错误：$1" >&2
-  exit 1
-}
-
-trap 'echo "脚本在 $LINENO 行出错，命令：$BASH_COMMAND" >&2; exit 1' ERR
+exec 1>/dev/null
 
 if [ -n "$TERMUX_VERSION" ]; then
-  echo '正在添加 TUR '
-  pkg install -y tur-repo || error_exit '无法添加 TUR'
+  echo '正在添加 TUR'
+  pkg install -y tur-repo
   echo '正在安装基本工具包（此过程耗时较长）'
-  pkg install -y termux-api git fish git-delta fastfetch eza zoxide bat fd ripgrep starship fzf jq htop yazi file stow lazygit neovim zellij build-essential nodejs-lts || error_exit '无法安装软件包'
+  pkg install -y termux-api git fish git-delta fastfetch eza zoxide bat fd ripgrep starship fzf jq htop yazi file stow lazygit neovim zellij build-essential nodejs-lts
 
   echo '正在建立配置文件软链接'
-  stow -t "$HOME" */ --adopt || error_exit '正在建立配置文件软链接'
+  stow -t "$HOME" */ --adopt
 
   echo '正在构建 Bat 缓存'
-  bat cache --build || error_exit '无法构建 Bat 缓存'
+  bat cache --build
 
   mkdir -p "$HOME/projects" && touch "$HOME/.hushlogin"
 
@@ -26,7 +20,7 @@ if [ -n "$TERMUX_VERSION" ]; then
     mkdir -p "$HOME/.img"
 
     echo '正在获取背景图片'
-    curl -o "$HOME/.img/back.jpg" https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/main/os/android-black-4k.png || error_exit '无法获取背景图片'
+    curl -fsSL "$HOME/.img/back.jpg" https://raw.githubusercontent.com/zhichaoh/catppuccin-wallpapers/main/os/android-black-4k.png
   fi
 
   echo '正在切换默认 Shell'
